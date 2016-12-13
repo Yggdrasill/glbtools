70a98177bf436b4be7eb7c932e911894e64dd0a2
Yggdrasill <yggdrasill@shitpost.is>
2016-12-13 15:18:26 +0100

    Make DEFAULT_KEY a const char * for indexing
            This simply makes DEFAULT_KEY a const char * so that it can be
            indexed. It also removes DEFAULT_KEY_LEN.

--- glbextract.h
+++ glbextract.h
@@ -1,8 +1,6 @@
 #define RAW_HEADER "\x64\x9B\xD1\x09"
-#define DEFAULT_KEY "32768GLB"
 
 #define RAW_HEADER_LEN (sizeof(RAW_HEADER) - 1)
-#define DEFAULT_KEY_LEN (sizeof(DEFAULT_KEY) - 1)
 #define MAX_FILENAME_LEN 16
 
 struct FATable {
@@ -12,4 +10,4 @@ struct FATable {
   char filename[MAX_FILENAME_LEN];
 };
 
-
+const uint8_t DEFAULT_KEY = "32768GLB";
