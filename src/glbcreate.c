1bf2aadf5cf2f37234bc99ac7daf27523474114a
Yggdrasill <yggdrasill@shitpost.is>
2017-04-29 22:22:59 +0200

    Fix use of magic number to MAX_FILES

--- src/glbcreate.c
+++ src/glbcreate.c
@@ -126,7 +126,7 @@ int main(int argc, char **argv)
 
   if(num_files <= 0) {
     term(ERR_NOFILE);
-  } else if(num_files >= 4096) {
+  } else if(num_files >= MAX_FILES) {
     term(ERR_TMFILE);
   }
 
