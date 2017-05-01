cdfa86b400eb2240a211673a4258db3f43d32340
Yggdrasill <yggdrasill@shitpost.is>
2017-05-01 18:37:26 +0200

    Rebrand to 'glbtools'

--- include/glbextract.h
+++ include/glbextract.h
@@ -1,20 +1,20 @@
 /*
  * Copyright (C) 2016-2017 Yggdrasill (kaymeerah@lambda.is)
  *
- * This file is part of glbextract.
+ * This file is part of glbtools.
  *
- * glbextract is free software: you can redistribute it and/or modify
+ * glbtools is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * glbextract is distributed in the hope that it will be useful,
+ * glbtools is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with glbextract.  If not, see <http://www.gnu.org/licenses/>.
+ * along with glbtools.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef GLBEXTRACT_H
